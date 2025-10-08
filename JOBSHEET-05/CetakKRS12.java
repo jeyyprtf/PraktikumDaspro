01a392c030e86585d6bf16ee57b000c201be0e94
Jujukaizen <juanmadhy425@gmail.com>
2025-10-08 08:35:57 +0700

    Percobaan 1

---
+++ JOBSHEET-05/CetakKRS12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CetakKRS12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS ---");
+        System.out.println("Apakah UKT sudah dibayar? (true/false)");
+        boolean uktLunas = sc.nextBoolean();
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi!");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+        sc.close();
+    }
+}
