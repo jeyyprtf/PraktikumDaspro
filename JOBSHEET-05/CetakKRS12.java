2d00c85e6defaa4cfec879a4972c4875088ce246
Jujukaizen <juanmadhy425@gmail.com>
2025-10-08 08:37:26 +0700

    Percobaan 1

--- JOBSHEET-05/CetakKRS12.java
+++ JOBSHEET-05/CetakKRS12.java
@@ -3,7 +3,7 @@ public class CetakKRS12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS ---");
-        System.out.println("Apakah UKT sudah dibayar? (true/false)");
+        System.out.print("Apakah UKT sudah dibayar? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi!");
