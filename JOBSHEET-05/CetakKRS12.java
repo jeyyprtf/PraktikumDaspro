c09ffaf7f2df0ba5718d63ada69054324823a5c3
Jujukaizen <juanmadhy425@gmail.com>
2025-10-08 09:42:32 +0700

    Percobaan 1 Ternary

--- JOBSHEET-05/CetakKRS12.java
+++ JOBSHEET-05/CetakKRS12.java
@@ -5,12 +5,8 @@ public class CetakKRS12 {
         System.out.println("--- Cetak KRS ---");
         System.out.print("Apakah UKT sudah dibayar? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi!");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("“Registrasi ditolak. Silakan lunasi UKT terlebih dahulu”");
-        }
+        String message = uktLunas ? "Pembayaran UKT terverifikasi!\nSilahkan cetak KRS dan minta tanda tangan DPA" : "“Registrasi ditolak. Silakan lunasi UKT terlebih dahulu”";
+        System.out.println(message);
         sc.close();
     }
 }
