6e14e553b4aa49e42d67251f2bbb93724a84733d
Jujukaizen <juanmadhy425@gmail.com>
2025-10-08 09:36:58 +0700

    Modifikasi Percobaan 1

--- JOBSHEET-05/CetakKRS12.java
+++ JOBSHEET-05/CetakKRS12.java
@@ -8,6 +8,8 @@ public class CetakKRS12 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi!");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("“Registrasi ditolak. Silakan lunasi UKT terlebih dahulu”");
         }
         sc.close();
     }
