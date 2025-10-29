c5548cf25296ea0c072c8088c7c177ab74a90796
Jujukaizen <juanmadhy425@gmail.com>
2025-10-29 09:12:00 +0700

    update jumlah mahasiswa lulu - jobsheet 7

--- JOBSHEET-07/SiakadForNoAbsen.java
+++ JOBSHEET-07/SiakadForNoAbsen.java
@@ -4,6 +4,7 @@ public class SiakadForNoAbsen {
         Scanner sc = new Scanner(System.in);
 
         double nilai,tertinggi = 0, terendah = 100;
+        int mahasiswaLulus = 0, mahasiswaTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,9 +16,15 @@ public class SiakadForNoAbsen {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            
+            if (nilai < 60) {
+                mahasiswaTidakLulus++;
+                mahasiswaLulus = (10-mahasiswaTidakLulus);
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Dari 10 mahasiswa, " + mahasiswaLulus + " diantaranya lulus, dan " + mahasiswaTidakLulus + " mahasiswa tidak lulus.");
         sc.close();
     }
 }
