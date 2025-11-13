1bbd60e569ecfa0052928597e4fbc360941d4430
Jujukaizen <juanmadhy425@gmail.com>
2025-11-13 20:47:53 +0700

    2.1 - percobaan 1

--- JOBSHEET-08/Tugas2.java
+++ JOBSHEET-08/Tugas2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Tugas2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan nilai n (minimal 3): ");
