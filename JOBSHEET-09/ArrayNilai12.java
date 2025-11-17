a4ff83ef5d3fec24dcc7470812f3915c45ebfc9c
Jujukaizen <juanmadhy425@gmail.com>
2025-11-17 10:39:11 +0700

    ArrayNilai

---
+++ JOBSHEET-09/ArrayNilai12.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ArrayNilai12 {
+    public static void main(String[] args) {
+        int[] nilaiAkhir = new int[10];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir untuk mahasiswa ke-" + (i + 1) + ": ");
+            nilaiAkhir[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.println("Nilai akhir mahasiswa ke-" + (i + 1) + " adalah: " + nilaiAkhir[i]);
+        }
+    }
+}
