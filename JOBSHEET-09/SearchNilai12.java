6e287d7bd8cedc1410c85cd188bdeab7b8c727c7
Jujukaizen <juanmadhy425@gmail.com>
2025-11-17 19:08:31 +0700

    soal ke 3

--- JOBSHEET-09/SearchNilai12.java
+++ JOBSHEET-09/SearchNilai12.java
@@ -1,17 +1,39 @@
+import java.util.Scanner;
+
 public class SearchNilai12 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 0; i < arrNilai.length; i++){
-            if (key == arrNilai[i]){
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = input.nextInt();
+
+        int[] arrNilai = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = input.nextInt();
+
+        int hasil = -1; 
+
+        for (int i = 0; i < n; i++) {
+            if (arrNilai[i] == key) {
                 hasil = i;
                 break;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ditemukan pada indeks ke-" + hasil);
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+
         System.out.println();
     }
 }
