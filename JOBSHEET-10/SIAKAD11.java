1db58900eb7a6f575395c5009b166c67b5eeb458
Jujukaizen <juanmadhy425@gmail.com>
2025-11-22 14:38:52 +0700

    penambahan fitur: dinamisasi panjang variabel array

--- JOBSHEET-10/SIAKAD11.java
+++ JOBSHEET-10/SIAKAD11.java
@@ -4,33 +4,39 @@ public class SIAKAD11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int nilai[][] = new int[4][3];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahSiswa = scanner.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = scanner.nextInt();
+
+        int nilai[][] = new int[jumlahSiswa][jumlahMatkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("\nInput nilai mahasiswa ke-" + (i+1));
             double totalPerSiswa = 0;
 
             for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + "Nilai mata kuliah" + (j+1) + ": ");
+                System.out.print("Masukkan nilai mata kuliah ke-" + (j+1) + ": ");
                 nilai[i][j] = scanner.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + (totalPerSiswa / 3));
+            System.out.println("Nilai rata-rata: " + (totalPerSiswa / jumlahMatkul));
         }
 
         System.out.println("\n-----------------------------------");
         System.out.println("Rata-rata nilai setiap mata kuliah");
 
-        for (int j = 0; j < nilai[0].length; j++) {
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMataKuliah = 0;
 
-            for (int i = 0; i < nilai.length; i++) {
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalPerMataKuliah += nilai[i][j];
             }
 
-            System.out.println("Mata kuliah " + (j+1) + ": " + (totalPerMataKuliah / 4));
+            System.out.println("Mata kuliah " + (j+1) + ": " + (totalPerMataKuliah / jumlahSiswa));
         }
-        scanner.close(); 
+        scanner.close();
     }
 }
