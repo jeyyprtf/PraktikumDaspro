3480369e6f13f87dfcd970046016dad167e94f16
Jujukaizen <juanmadhy425@gmail.com>
2025-11-22 14:24:12 +0700

    SIAKAD 1.0

---
+++ JOBSHEET-10/SIAKAD11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SIAKAD11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nilai[][] = new int[4][3];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i+1));
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + "Nilai mata kuliah" + (j+1) + ": ");
+                nilai[i][j] = scanner.nextInt();
+            }
+        }
+    }
+}
